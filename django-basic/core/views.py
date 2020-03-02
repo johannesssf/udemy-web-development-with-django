06cde67a48f017a19047c80e47d928d04991b892
Johannes Ferreira <johannesssf@gmail.com>
2020-03-02 16:15:25 -0300

    Django basic final version

--- django-basic/core/views.py
+++ django-basic/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 
 from .models import Product
 
@@ -19,9 +21,30 @@ def contact(request):
 
 
 def product(request, id):
-    prod = Product.objects.get(id=id)
+    # prod = Product.objects.get(id=id)
+    prod = get_object_or_404(Product, id=id)
     context = {
         'product': prod
     }
 
     return render(request, 'product.html', context)
+
+
+def error404(request, exception):
+    # return render(request, '404.html')
+    template = loader.get_template('404.html')
+    return HttpResponse(
+        content=template.render(),
+        content_type='text/html; charset=utf8',
+        status=404,
+    )
+
+
+def error500(request):
+    # return render(request, '500.html')
+    template = loader.get_template('500.html')
+    return HttpResponse(
+        content=template.render(),
+        content_type='text/html; charset=utf8',
+        status=500,
+    )
--- django-basic/django_basic/settings.py
+++ django-basic/django_basic/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b16ex4$67ihk18p3rh++q_m8j&+x1g*75ddrlz0x^$)y01(l57'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+LOGOUT_REDIRECT_URL = 'index'
